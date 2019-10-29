0a64f2e52cb4f9ec6e7355b442b3d2c4b903416f
Ben Conway <benrconway84@gmail.com>
2019-10-29 15:51:30 +0000

    Added signout functionality

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, View, StyleSheet } from 'react-native';
 import firebase from 'firebase'
-import { Header } from './src/components/common'
+import { Header, Button, Spinner } from './src/components/common'
 import LoginForm from './src/components/LoginForm'
 import { firebaseConfig } from './firebaseConfig';
 
 class App extends Component{
+  state = { loggedIn : null }
   componentDidMount() {
     !firebase.apps.length? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
+  renderContent() {
+    switch(this.state.loggedIn){
+      case true:
+        return(
+          <Button onPress={this.logout.bind(this)}>
+            Log Out
+          </Button>
+        )
+      case false:
+        return <LoginForm />
+      default:
+        return <Spinner />
+    }
   }
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <Header headerText="Auth App" />
-        <LoginForm />
+        { this.renderContent() }
       </SafeAreaView>
     )
   }
@@ -23,7 +51,7 @@ class App extends Component{
 const styles = StyleSheet.create({
   container:{
     flex: 1
-  }
+  },
 });
 
 export default App;
